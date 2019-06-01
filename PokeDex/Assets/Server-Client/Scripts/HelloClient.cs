e65e537b9996da5ea56dc42082b935e54c76dedb
MohanadZ <mzeito14@student.aau.dk>
2019-06-01 10:16:42 +0200

    Unity project + connection with Python

---
+++ PokeDex/Assets/Server-Client/Scripts/HelloClient.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using AsyncIO;
+using NetMQ;
+using NetMQ.Sockets;
+
+public class HelloClient : MonoBehaviour
+{
+    private HelloRequester _helloRequester;
+    public bool SendPack = true;
+
+    void Update()
+    {
+        if (SendPack)
+        {
+            _helloRequester.Continue();
+        } else if (!SendPack)
+        {
+            _helloRequester.Pause();
+        }
+    }
+
+    private void Start()
+    {
+        _helloRequester = new HelloRequester();
+        _helloRequester.Start();
+    }
+
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+}
